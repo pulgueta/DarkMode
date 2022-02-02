5c85dba5a0f89e253a25d1d75fd6d5cefa42462e
Andrés <retardix456@gmail.com>
2022-02-02 16:12:32 -0500

    ss

---
+++ src/components/UI.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './ui.css'
+
+const UI = () => {
+  return (
+    <div className='dark'>
+      <div className='card'>
+        
+      </div>
+    </div>
+  );
+};
+
+export default UI;
