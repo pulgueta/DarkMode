66fbcd97b8408aed3d18291fc7b32c422331a54e
Andrés <retardix456@gmail.com>
2022-02-02 21:38:26 -0500

    final

--- src/components/UI.jsx
+++ src/components/UI.jsx
@@ -30,17 +30,23 @@ const UI = () => {
         <div className='details'>
           <h2>Main details:</h2>
           <br />
-          <ul>
-            <li><p style={style}>The employee uses the MERN stack to develop the applications our clients need. Is also open to use other backend languages as 
-            Python, Ruby, C# or Golang.
+          <ul style={{ borderBottom: '1px solid rgba(167, 167, 167, 0.5)' }}>
+            <li><p style={style}>The employee uses the MERN stack to develop the applications our clients need. Is also open to use other backend languages as
+              Python, Ruby, C# or Golang.
             </p></li>
             <li><p style={style}>
               This is one of the top performers in the whole team, we are going to give the employee an outstanding bootcamp to use the Angular framework, as well
               as an extensive training in TypeScript.
             </p></li>
+            <li>
+              <p style={style}>
+                By mid-june, the employee will be part of an exchange with an American university to have the opportunity to travel to the United States of America
+                and get more professional relationships.
+              </p>
+            </li>
           </ul>
           <div className='icons'>
-            <FaReact fontSize='large'/>
+            <FaReact fontSize='large' />
             <FaPython fontSize='large' />
             <DiRuby fontSize='large' />
             <SiCsharp fontSize='large' />
@@ -50,7 +56,7 @@ const UI = () => {
 
         </div>
       </div>
-    </div>
+    </div >
   );
 };
 
